7b9c253bc807c0e1a9e94ae233914640e12790d6
Shashikant <yadavshashikant884@gmail.com>
2025-03-06 01:10:40 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
