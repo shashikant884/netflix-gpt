f86738e183decb58a6f8437828e18390ad403e48
Shashikant <yadavshashikant884@gmail.com>
2025-03-06 19:15:50 +0530

    SignIn/SignUp login

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
-npm create vite@latest netflix-gpt -- --template react
-install tailwind amd configure 
+-npm create vite@latest netflix-gpt -- --template react
+-install tailwind amd configure 
     -npm install -D tailwindcss@3 postcss autoprefixer
     -npx tailwindcss init -p
-npm run dev
+-npm run dev
 
-install : npm i react-router-dom
+-install : npm i react-router-dom
 
 When the app loads at /, the Login component is shown.
 If the user navigates to /browse, the Browse component is displayed.
 The RouterProvider makes sure the correct component is rendered based on the URL.
 
-Hearder
-Login form
+-Hearder
+-Login form
+-Sign In
+-Sign Up
+
+🔹 How It Works
+useState(true)
+
+true → Sign In form is shown by default.
+false → Sign Up form is shown.
+Toggle Between Forms
+
+When clicking "New to Netflix? Sign Up Now", setIsSignIn(!isSignIn) toggles the state.
+If isSignIn === false, it changes to "Already have an account? Sign In".
+Dynamic Content
+
+The title (Sign In / Sign Up) and button text update dynamically.
+The Full Name input is only shown for Sign Up
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+
+    const [isSignIn , setIsSignIn] = useState(true);
+
     return(
         <div>
             <div>
@@ -11,10 +15,14 @@ const Login = () => {
             />
             </div>
             <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-                <h1 className="font-bold text-3xl py-3">Sing In</h1>
+                <h1 className="font-bold text-3xl py-3">{isSignIn ? "Sign In" : "Sign Up"}</h1>
+                {!isSignIn && ( 
+                    <input type="text" placeholder="Name " className="p-4 my-4 w-full bg-gray-700"/>
+                    )}
                 <input type="text" placeholder="Email Address " className="p-4 my-4 w-full bg-gray-700"  />
                 <input type="password" placeholder="Password " className="p-4 my-4 w-full bg-gray-700" />
-                <button className="p-4 my-6 bg-red-700 w-full rounded-lg">Sing In</button>
+            <button className="p-4 my-6 bg-red-700 w-full rounded-lg">{isSignIn ? "Sign In" : "Sign Up"}</button>
+                <p className="py-4 cursor-pointer" onClick={()=>{setIsSignIn(!isSignIn)}}>{isSignIn ? "New to Netflix? Sign Up Now" : "Already Have accunt? Sign In" }</p>
 
             </form>
         </div>
